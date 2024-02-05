b050814702f1af478c05b1ca84d97fbe00aaad70
Vitovszki Tamas <lolozol80xd@gmail.com>
2024-02-05 11:17:11 +0100

    working "minimap"

--- Horgaszhely/src/app/parts/book-main/book-main.component.ts
+++ Horgaszhely/src/app/parts/book-main/book-main.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ViewportScroller } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-book-main',
@@ -8,36 +9,42 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class BookMainComponent {
   spotInfo: any
-  
-  parentContainerWidth = 1920; // Update this with the actual width
-  parentContainerHeight = 1080; // Update this with the actual height
 
-  constructor(private modalService: NgbModal) {}
+
+  parentContainerWidth: number = window.innerWidth
+  parentContainerHeight: number = window.innerHeight
 
   locations = [
-    { id: 1, leftPercentage: (420 / this.parentContainerWidth) * 100, topPercentage: (200 / this.parentContainerHeight) * 100 },
-    { id: 2, leftPercentage: (200 / this.parentContainerWidth) * 100, topPercentage: (560 / this.parentContainerHeight) * 100 },
+    { id: 1, leftPercentage: (680 / this.parentContainerWidth) * 100, topPercentage: (200 / this.parentContainerHeight) * 100 },
+    { id: 2, leftPercentage: (230 / this.parentContainerWidth) * 100, topPercentage: (560 / this.parentContainerHeight) * 100 },
     { id: 3, leftPercentage: (390 / this.parentContainerWidth) * 100, topPercentage: (700 / this.parentContainerHeight) * 100 },
-    { id: 4, leftPercentage: (820 / this.parentContainerWidth) * 100, topPercentage: (760 / this.parentContainerHeight) * 100 },
-    { id: 5, leftPercentage: (1040 / this.parentContainerWidth) * 100, topPercentage: (615 / this.parentContainerHeight) * 100 },
-    { id: 6, leftPercentage: (1058 / this.parentContainerWidth) * 100, topPercentage: (293 / this.parentContainerHeight) * 100 },
+    { id: 4, leftPercentage: (820 / this.parentContainerWidth) * 100, topPercentage: (780 / this.parentContainerHeight) * 100 },
+    { id: 5, leftPercentage: (1600 / this.parentContainerWidth) * 100, topPercentage: (615 / this.parentContainerHeight) * 100 },
+    { id: 6, leftPercentage: (1580 / this.parentContainerWidth) * 100, topPercentage: (293 / this.parentContainerHeight) * 100 },
   ];
 
+  @ViewChild('imageContainer') imageContainer!: ElementRef
+
+  constructor(private modalService: NgbModal, private viewportScroller: ViewportScroller) { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+
+    this.parentContainerWidth = window.innerWidth;
+    this.parentContainerHeight = window.innerHeight;
+    console.log(this.parentContainerHeight)
+    console.log(this.parentContainerWidth)
+  }
 
   openSpotInfoModal(spotId: number) {
-    // Set spot information before displaying the modal
     this.spotInfo = {
       helySzama: `Spot ${spotId}`,
       helyLeirasa: `Description for Spot ${spotId}`,
-      slideshowImages: ['image1.jpg', 'image2.jpg', 'image3.jpg']
-      // Add more fields as needed
+      slideshowImages: ['../../../assets/images/nomnom.jpg', '../../../assets/images/therapist.jpg', '../../../assets/images/giganigga.jpg']
     };
   }
 
-  // Optional: Function to close the modal
   closeSpotInfoModal() {
     this.spotInfo = null;
   }
-
-
 }
