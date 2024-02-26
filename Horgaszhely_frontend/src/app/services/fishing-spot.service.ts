84d549a0660535f9de57c1ca5a8f2adaeb55e0f6
Vito <lolozol80xd@gmail.com>
2024-02-26 08:54:48 +0100

    ongoing

--- Horgaszhely_frontend/src/app/services/fishing-spot.service.ts
+++ Horgaszhely_frontend/src/app/services/fishing-spot.service.ts
@@ -1,116 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FishingSpotService {
-  getFishingSpots(): Observable<FishingSpot[]> {
-    const fishingSpots: FishingSpot[] = [
-      {
-        reservable: true,
-        pier: false,
-        firepit: true,
-        shelter: false,
-        averageRating: 4,
-        description: 'Great fishing spot!',
-        longitude:  19.572352,
-        latitude: 47.536311,
-        images: [
-          "../../../assets/images/giganigga.jpg",
-          "../../../assets/images/hogrider.jpg",
-          "../../../assets/images/stepmom.jpg"
-        ]
-      },
-      {
-        reservable: true,
-        pier: false,
-        firepit: true,
-        shelter: false,
-        averageRating: 4,
-        description: 'Great fishing spot!',
-        longitude: 19.572169,
-        latitude: 47.534790,
-        images: [
-          "../../../assets/images/giganigga.jpg",
-          "../../../assets/images/hogrider.jpg",
-          "../../../assets/images/stepmom.jpg"
-        ]
-      },
-      {
-        reservable: true,
-        pier: false,
-        firepit: true,
-        shelter: false,
-        averageRating: 4,
-        description: 'Great fishing spot!',
-        longitude: 19.570300,
-        latitude: 47.535814,
-        images: [
-          "../../../assets/images/giganigga.jpg",
-          "../../../assets/images/hogrider.jpg",
-          "../../../assets/images/stepmom.jpg"
-        ]
-      },
-      {
-        reservable: true,
-        pier: false,
-        firepit: true,
-        shelter: false,
-        averageRating: 4,
-        description: 'Great fishing spot!',
-        longitude: 19.567223,
-        latitude: 47.537463,
-        images: [
-          "../../../assets/images/giganigga.jpg",
-          "../../../assets/images/hogrider.jpg",
-          "../../../assets/images/stepmom.jpg"
-        ]
-      },
-      {
-        reservable: true,
-        pier: false,
-        firepit: true,
-        shelter: false,
-        averageRating: 4,
-        description: 'Great fishing spot!',
-        longitude: 19.566816,
-        latitude: 47.538346,
-        images: [
-          "../../../assets/images/giganigga.jpg",
-          "../../../assets/images/hogrider.jpg",
-          "../../../assets/images/stepmom.jpg"
-        ]
-      },
-      {
-        reservable: true,
-        pier: false,
-        firepit: true,
-        shelter: false,
-        averageRating: 4,
-        description: 'Great fishing spot!',
-        longitude: 19.574025,
-        latitude: 47.535666,
-        images: [
-          "../../../assets/images/giganigga.jpg",
-          "../../../assets/images/hogrider.jpg",
-          "../../../assets/images/stepmom.jpg"
-        ]
-      }
-    ];
+  private apiUrl = 'localhost:8000';
 
-    return of(fishingSpots);
+  constructor(private http: HttpClient) { }
+
+  getFishingSpots(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/fishingplaces`);
   }
-}
 
-export interface FishingSpot {
-  reservable: boolean;
-  pier: boolean;
-  firepit: boolean;
-  shelter: boolean;
-  averageRating: number;
-  description: string;
-  longitude: number;
-  latitude: number;
-  images: string[];
 }
