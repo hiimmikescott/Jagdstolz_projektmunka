2ef9899004e01be44d58abe34e9aa025cefe77c5
Vitovszki Tamas <lolozol80xd@gmail.com>
2024-02-28 12:01:10 +0100

    asd

--- Horgaszhely_frontend/src/app/services/auth.service.ts
+++ Horgaszhely_frontend/src/app/services/auth.service.ts
@@ -46,6 +46,23 @@ export class AuthService {
     return this.http.post('/userlogout', {}, { headers });
   }
 
+  private getCookieValue(cookieName: string): string {
+    const name = `${cookieName}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+
+    for (let i = 0; i < cookieArray.length; i++) {
+      let cookie = cookieArray[i];
+      while (cookie.charAt(0) === ' ') {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(name) === 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
+    }
+    return '';
+  }
+
   isLoggedIn() {
     return this.loggedIn;
   }
