178d7ff674fffcf1a93b85425a7f886b792f0b4f
Vitovszki Tamas <lolozol80xd@gmail.com>
2024-02-28 12:09:24 +0100

    asd

--- Horgaszhely_frontend/src/app/parts/login-modal/login-modal.component.ts
+++ Horgaszhely_frontend/src/app/parts/login-modal/login-modal.component.ts
@@ -6,29 +6,34 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
-  styleUrl: './login-modal.component.css'
+  styleUrls: ['./login-modal.component.css']
 })
 export class LoginModalComponent {
-  constructor(public activeModal: NgbActiveModal, private auth : AuthService, private router: Router) {}
-  email:any
-  password:any
+  constructor(public activeModal: NgbActiveModal, private auth: AuthService, private router: Router) {}
+
+  email: any;
+  password: any;
+
   submitForm() {
     this.activeModal.close();
   }
-  login(){
-    
+
+  login() {
     this.auth.login(this.email, this.password).subscribe(
-{      next:(loginResponse:any) => {
-        const domain = window.location.hostname;
-        const path = window.location.pathname;
-        this.router.navigate(['/home']);
-        this.auth.loggedIn=true
-        this.auth.setCookie('userToken', loginResponse.data.token, 7, domain, path);
-        console.log(loginResponse)
-      },
-      error:(loginError) => {
-        console.error('Login failed after registration', loginError);
-      }}
+      {
+        next: (loginResponse: any) => {
+          const domain = window.location.hostname;
+          const path = window.location.pathname;
+          this.router.navigate(['/home']);
+          this.auth.loggedIn = true;
+          this.auth.clearUserData();
+          this.auth.cookieService.set('auth_token', loginResponse.data.token, 7, domain, path);
+          console.log(loginResponse);
+        },
+        error: (loginError) => {
+          console.error('Login failed after registration', loginError);
+        }
+      }
     );
   }
 }
