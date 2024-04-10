9a3eb5d2de1c82d8f27450ff4d3e3ee86bd644e5
hiimmikescott <szekeresmiklos@ktch.hu>
2024-04-10 17:24:29 +0200

    asd

--- horgaszhely_backend/app/Http/Controllers/ReservationController.php
+++ horgaszhely_backend/app/Http/Controllers/ReservationController.php
@@ -116,17 +116,21 @@ class ReservationController extends ResponseController
         }
 
         //---{  success  }-------------
+        if ($this->testDate($input)) {
+            $reservation-> user_id=$input["user_id"];
+            $reservation-> fishingplace_id=$input["fishingplace_id"];
+            $reservation-> reservationStart=$input["reservationStart"];
+            $reservation-> reservationEnd=$input["reservationEnd"];
+            $reservation-> guestNumber=$input["guestNumber"];
+            
+            $reservation->save();
+            
+            return  $this->sendResponse($reservation,"Foglalás módosítva");
+        }else{
+            return  $this->sendError("Erre az időpontra már foglalt a hely"); 
+        }
 
-        $reservation-> user_id=$input["user_id"];
-        $reservation-> fishingplace_id=$input["fishingplace_id"];
-        $reservation-> reservationStart=$input["reservationStart"];
-        $reservation-> reservationEnd=$input["reservationEnd"];
 
-        $reservation-> guestNumber=$input["guestNumber"];
-
-        $reservation->save();
-
-        return  $this->sendResponse($reservation,"Foglalás módosítva");
 
     }
 
